b662c26500e324ac850cf81c8014bd1cf429898b
Preston Ong <preston4896@me.com>
2025-02-04 18:16:06 +0800

    program id

--- solana-verifier/programs/groth_16_verifier/src/lib.rs
+++ solana-verifier/programs/groth_16_verifier/src/lib.rs
@@ -29,7 +29,7 @@ pub mod client;
 
 pub use vk::{VerificationKey, VERIFICATION_KEY};
 
-declare_id!("CA4wbHHhe4bxakLB4t21uQKWSjpGYciNKJjJS26XRnmi");
+declare_id!("5HrF6mJAaSFdAym2xZixowzVifPyyzTuTs3viYKdjy4s");
 
 // Base field modulus 'q' for BN254
 // https://docs.rs/ark-bn254/latest/ark_bn254/
